de72edf1501938093fc266f296e149427b037b27
diaquino <25682@ma-web.nl>
2020-04-03 10:33:20 +0200

    feature/layout
    Ik heb de layout zo gemaakt dat elke pagina het overneemt en dat de hele body een @yield is zodat de content per pagina anders en, maar de <head> op elke pagina hetzelfde blijft.

--- resources/views/layouts/general.blade.php
+++ resources/views/layouts/general.blade.php
@@ -4,36 +4,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, user-scalable=no ,initial-scale=1, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Boosted Delivery</title>
+    <title>
+      @section('title')
+        Boosted Delivery
+      @show
+    </title>
 
     <!-- Styles -->
     <link rel="stylesheet" href="<?php echo asset('/css/app.css')?>">
   </head>
   <body>
-    <header>
-      <h2>
-        @section('header')
-          Boosted Delivery
-        @show
-      </h2>
-    </header>
-
-    <nav>
-      @section('nav')
-        <a href="{{ url('/') }}">Home</a>
-        <a href="{{ url('/producten') }}">Producten</a>
-        <a href="{{ url('/abonnementen') }}">Abonnementen</a>
-      @show
-    </nav>
-
-    <main>
-      @yield('content')
-    </main>
-
-    <footer>
-      @section('footer')
-        Boosted Footer
-      @show
-    </footer>
+    @yield('body')
   </body>
 </html>
