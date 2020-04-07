84f9056192a30784edaf12f6013110fff4d8efa9
diaquino <25682@ma-web.nl>
2020-04-07 15:21:02 +0200

    other functions added and removed

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Product;
 
 class ProductController extends Controller
 {
@@ -13,10 +15,10 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return view('producten');
+
     }
 
-    public function abonnementen()
+    public function subscriptions()
     {
         return view('abonnementen');
     }
@@ -28,7 +30,7 @@ class ProductController extends Controller
      */
     public function create()
     {
-        
+        return view('product.form');
     }
 
     /**
@@ -40,14 +42,57 @@ class ProductController extends Controller
     public function store(Request $request)
     {
         
+        $postData = $request->validate(
+            [
+                'title' => 'required|min:3|max:255',
+                'content' => 'required|min:10',
+                'pub_date' => 'required|after_or_equal:today',
+                'image' => 'image'
+            ]
+        );
+
+        $newFileName = $postData['image']->store('img', 'public');
+        $postData['image'] = $newFileName;
+
+        Product::create($postData);
+
+        return redirect()->route('blog.list');
     }
 
+    /*
+     * Displays all posts
+     */
+    public function list()
+    {
+        $posts = Product::orderBy('created_at', 'desc')->paginate(20);
+
+        return view('blog.list', ['posts'=> $posts]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function detail($id)
+    {
+        $post = Product::find($id);
+        return view('product.detail', ['fullproduct'=> $post]);
+    }
+
+    /*
+     * Gets the keuze from the abonnementen page and sends it to the overzicht page to use the switch cases
+     */
     public function showChoice(Request $request) {
       $keuze = $request->input('keuze');
 
       return view('abo_overzicht', compact('keuze'));
     }
 
+    /*
+     * goes to the overzicht page
+     */
     public function showOverview()
     {
         return view('abo_overzicht');
