07415099a6fecce5287869618c8c27552a9f4380
diaquino <25682@ma-web.nl>
2020-04-16 12:32:54 +0200

    feature/nav
    profiel en product toevoegen, toegevoegd aan de navigatie

--- resources/views/layouts/general.blade.php
+++ resources/views/layouts/general.blade.php
@@ -70,7 +70,8 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('product.add') }}">{{ __('Toevoegen') }}</a>
+                            <a class="dropdown-item" href="{{ route('profiel') }}">{{ __('Dashboard') }}</a>
+                            <a class="dropdown-item" href="{{ route('product.add') }}">{{ __('Add') }}</a>
                             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
