3e29c0f09075ec5b09327f857d4755189d8417ea
osma58 <27050@ma-web.nl>
2020-06-04 17:37:16 +0200

    dasboard update

--- resources/views/profiel.blade.php
+++ resources/views/profiel.blade.php
@@ -1,11 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+
+        <div class="dash row">
+            <div class="dash-menu">
+                <h3 class="dash-menu-text">Abonnement aanpassen</h3>
+                <h3 class="dash-menu-text">Product toevoegen</h3>
+                <h3 class="dash-menu-text">Wachtwoord resetten</h3>
+                <h3 class="dash-menu-text">Uitloggen</h3>
+            </div>
+            <div class="vertical-line"></div>
+            <div class="vertical-line-mobile"></div>
+            <div class="dash-info">
+                <h1>Welkom, [Gebruikersnaam]</h1>
+                <hr class="hr2">
+                <h3><span>-</span> Abonnement: Boosted PowerCrate (<span>€4.49</span>)</h3>
+                <h3><span>-</span> Dit is uw eerste week </h3>
+                <h3><span>-</span> Dit is een wekelijks abonnement </h3>
+                <div class="h4-4"><h3 ><span>-</span> U heeft de volgende energie drankjes gekozen in uw Crate:</h3></div>
+
+                <h3 class="drinks"><span>></span> x1 Redbull</h3>
+                <h3 class="drinks"><span>></span> x2 Monster</h3>
+                <h3 class="drinks"><span>></span> x3 Slammers</h3>
+                <h3 class="drinks"><span>></span> x1 Bling</h3>
+            </div>
+        </div>
+
+        
+@endsection
+
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+           
+                
+            
+        </div>
+    </div>
+</div> --}}
+{{-- <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -15,9 +49,12 @@
                     @endif
 
                     You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+                </div> --}}
+
+
+                
+
+
+                
+
+            
