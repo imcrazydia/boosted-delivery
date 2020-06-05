4cff5fcf3f18530c13a41449bc03bda66f68ef30
diaquino <25682@ma-web.nl>
2020-06-05 07:12:42 +0200

    feature/profile
    added linking to the menu

--- resources/views/profiel.blade.php
+++ resources/views/profiel.blade.php
@@ -1,33 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-
-
-        <div class="dash row">
-            <div class="dash-menu">
-                <h3 class="dash-menu-text">Abonnement aanpassen</h3>
-                <h3 class="dash-menu-text">Product toevoegen</h3>
-                <h3 class="dash-menu-text">Wachtwoord resetten</h3>
-                <h3 class="dash-menu-text">Uitloggen</h3>
-            </div>
-            <div class="vertical-line"></div>
-            <div class="vertical-line-mobile"></div>
-            <div class="dash-info">
-                <h1>Welkom, [Gebruikersnaam]</h1>
-                <hr class="hr2">
-                <h3><span>-</span> Abonnement: Boosted PowerCrate (<span>€4.49</span>)</h3>
-                <h3><span>-</span> Dit is uw eerste week </h3>
-                <h3><span>-</span> Dit is een wekelijks abonnement </h3>
-                <div class="h4-4"><h3 ><span>-</span> U heeft de volgende energie drankjes gekozen in uw Crate:</h3></div>
-
-                <h3 class="drinks"><span>></span> x1 Redbull</h3>
-                <h3 class="drinks"><span>></span> x2 Monster</h3>
-                <h3 class="drinks"><span>></span> x3 Slammers</h3>
-                <h3 class="drinks"><span>></span> x1 Bling</h3>
-            </div>
+<div class="dashboard">
+    <div class="dash row">
+        <div class="dash-menu">
+            <a class="r-link menu__link" href="{{ route('abonnementen.overzicht') }}"><h3 class="dash-menu-text">Abonnement aanpassen</h3></a>
+            <a class="r-link menu__link" href="{{ route('product.add') }}"><h3 class="dash-menu-text">Product toevoegen</h3></a>
+            <a class="r-link menu__link" href="{{ route('product.add') }}"><h3 class="dash-menu-text">Wachtwoord resetten</h3></a>
+            <a class="r-link menu__link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                      document.getElementById('logout-form').submit();">
+                      <h3 class="dash-menu-text">Uitloggen</h3>
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                      @csrf
+                    </form>
+            
         </div>
+        <div class="vertical-line"></div>
+        <div class="vertical-line-mobile"></div>
+        <div class="dash-info">
+            <h1>Welkom, [Gebruikersnaam]</h1>
+            <hr class="hr2">
+            <h3><span>-</span> Abonnement: Boosted PowerCrate (<span>€4.49</span>)</h3>
+            <h3><span>-</span> Dit is uw eerste week </h3>
+            <h3><span>-</span> Dit is een wekelijks abonnement </h3>
+            <div class="h4-4">
+                <h3><span>-</span> U heeft de volgende energie drankjes gekozen in uw Crate:</h3>
+            </div>
 
-        
+            <h3 class="drinks"><span>></span> x1 Redbull</h3>
+            <h3 class="drinks"><span>></span> x2 Monster</h3>
+            <h3 class="drinks"><span>></span> x3 Slammers</h3>
+            <h3 class="drinks"><span>></span> x1 Bling</h3>
+        </div>
+    </div>
+</div>
 @endsection
 
 {{-- <div class="container">
@@ -45,16 +52,8 @@
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
-                        </div>
-                    @endif
+</div>
+@endif
 
-                    You are logged in!
-                </div> --}}
-
-
-                
-
-
-                
-
-            
+You are logged in!
+</div> --}}
--- storage/framework/views/09ba55f1a935be14c2763ec1fa3f4b4747ebcf14.php
+++ storage/framework/views/09ba55f1a935be14c2763ec1fa3f4b4747ebcf14.php
@@ -1,26 +1,43 @@
 
 
 <?php $__env->startSection('content'); ?>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    <?php if(session('status')): ?>
-                        <div class="alert alert-success" role="alert">
-                            <?php echo e(session('status')); ?>
-
-                        </div>
-                    <?php endif; ?>
-
-                    You are logged in!
-                </div>
+<div class="dashboard">
+    <div class="dash row">
+        <div class="dash-menu">
+            <a class="r-link menu__link" href="<?php echo e(route('abonnementen.overzicht')); ?>"><h3 class="dash-menu-text">Abonnement aanpassen</h3></a>
+            <a class="r-link menu__link" href="<?php echo e(route('product.add')); ?>"><h3 class="dash-menu-text">Product toevoegen</h3></a>
+            <a class="r-link menu__link" href="<?php echo e(route('product.add')); ?>"><h3 class="dash-menu-text">Wachtwoord resetten</h3></a>
+            <a class="r-link menu__link" href="<?php echo e(route('logout')); ?>" onclick="event.preventDefault();
+                      document.getElementById('logout-form').submit();">
+                      <h3 class="dash-menu-text">Uitloggen</h3>
+                    </a>
+                    <form id="logout-form" action="<?php echo e(route('logout')); ?>" method="POST" style="display: none;">
+                      <?php echo csrf_field(); ?>
+                    </form>
+            
+        </div>
+        <div class="vertical-line"></div>
+        <div class="vertical-line-mobile"></div>
+        <div class="dash-info">
+            <h1>Welkom, [Gebruikersnaam]</h1>
+            <hr class="hr2">
+            <h3><span>-</span> Abonnement: Boosted PowerCrate (<span>€4.49</span>)</h3>
+            <h3><span>-</span> Dit is uw eerste week </h3>
+            <h3><span>-</span> Dit is een wekelijks abonnement </h3>
+            <div class="h4-4">
+                <h3><span>-</span> U heeft de volgende energie drankjes gekozen in uw Crate:</h3>
             </div>
+
+            <h3 class="drinks"><span>></span> x1 Redbull</h3>
+            <h3 class="drinks"><span>></span> x2 Monster</h3>
+            <h3 class="drinks"><span>></span> x3 Slammers</h3>
+            <h3 class="drinks"><span>></span> x1 Bling</h3>
         </div>
     </div>
 </div>
 <?php $__env->stopSection(); ?>
+
+
+
 
 <?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\ma\bewijzenmap\periode2.3\k_id\boosted-delivery\resources\views/profiel.blade.php ENDPATH**/ ?>
