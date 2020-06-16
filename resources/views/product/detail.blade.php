f3df8026af22c0919a9c8dcce1d12432f2857d51
diaquino <25682@ma-web.nl>
2020-06-16 22:57:17 +0200

    feature/product_detail
    desktop styling done

--- resources/views/product/detail.blade.php
+++ resources/views/product/detail.blade.php
@@ -8,5 +8,25 @@
 
 <!-- This is where the body starts and since there is a yield in the layout you can just create a section and start adding content -->
 @section('content')
-
+<div class="product-detail row">
+  <div class="product-detail__list col-md-3 offset-md-1">
+    <h1 class="product-detail__list__title">Andere producten</h1>
+    <ul class="product-detail__list__wrapper">
+      <li class="product-detail__list__item">Monster <span class="product-detail__list__item__arrow">--></span></li>
+      <li class="product-detail__list__item">Shakura <span class="product-detail__list__item__arrow">--></span></li>
+      <li class="product-detail__list__item">Bang <span class="product-detail__list__item__arrow">--></span></li>
+      <li class="product-detail__list__item">Bullit <span class="product-detail__list__item__arrow">--></span></li>
+    </ul>
+  </div>
+  <div class="product-detail__info col-md-6">
+    <div class="row">
+      <img src="{{ asset('img/products/redbull.png') }}" alt="redbull" class="product-detail__info__image col-md-6" />
+      <div class="product-detail__info__content col-md-6">
+        <h1 class="product-detail__info__content__title">Red Bull</h1>
+        <p class="product-detail__info__content__description">Red Bull Energy Drink wordt wereldwijd gewaardeerd door topatleten, studenten en prestatiegerichte professionals. En Red Bull houdt je scherp tijdens lange autoritten.</p>
+        <p class="product-detail__info__content__ingredients">calorieën: 110, koolhydraten: 28g, sodium: 105mg, suiker: 27g</p>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
